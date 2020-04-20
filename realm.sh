5744012d540b2918343dabdb770f51d85e943b11
Martin Sauve <msauve@redhat.com>
2020-04-20 09:15:56 -0400

    initial commit

---
+++ realm.sh
@@ -0,0 +1,18 @@
+export SSO_HOST=https://sso-sso-app-demo.apps.cl1.myocp.net
+retrieve_token_url=$SSO_HOST/auth/realms/master/protocol/openid-connect/token
+
+
+TKN=$(curl -X POST "$retrieve_token_url" -k \
+            -H "Content-Type: application/x-www-form-urlencoded" \
+            -d "username=admin" \
+            -d "password=password" \
+            -d "grant_type=password" \
+            -d "client_id=admin-cli" \
+            | sed 's/.*access_token":"//g' | sed 's/".*//g')
+
+
+curl -v -X POST -k \
+  -H "Authorization: Bearer $TKN" \
+  -H "Content-Type: application/json" \
+  -d @realm.json \
+  $SSO_HOST/auth/admin/realms
